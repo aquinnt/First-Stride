477006eac462a4655804a02845227b939b3dc0dc
TBrvnson <167715585+TBrvnson@users.noreply.github.com>
2025-10-26 17:03:30 -0700

    Small Aesthetic Update to Workout Page
    Added Large buttons to open menus to enter workouts rather than direct to the page (Did not delete old method as I am unable to test new page works)

--- FirstStride/WorkoutsView.swift
+++ FirstStride/WorkoutsView.swift
@@ -14,29 +14,46 @@ struct WorkoutsView: View {
     @State private var dur: Int = 0
     @State private var dist: Double = 0.0
     @State private var tw: Bool = false
+    @State public var showSheet: Bool = false
+    @State public var showSheet2: Bool = false
     let store = FirestoreService()
 
     var body: some View {
+        //Opens Menu for adding workouts - Brvnson
+        Button(action: {
+            showSheet.toggle()
+        }, label: {
+            Text("Add Workout")
+                .foregroundStyle(.white)
+                .padding()
+                .background(Color.red)
+                .cornerRadius(10)
+        })
+        .sheet(isPresented: $showSheet, content: {
+            WorkoutPopup()
+        })
+        
+        //Opens Menu for adding timed workout - Brvnson
+        Button(action: {
+            showSheet2.toggle()
+        }, label: {
+            Text("Add Timed Workout")
+                .foregroundStyle(.white)
+                .padding()
+                .background(Color.red)
+                .cornerRadius(10)
+        })
+        .sheet(isPresented: $showSheet2, content: {
+            TimedWorkoutPopup()
+        })
+        
+        //Old workout adding menu
         VStack(spacing: 12) {
-            HStack {
-                Button("Add Sample Workout") {
-                    Task {
-                        guard let uid = Auth.auth().currentUser?.uid else { return }
-                        let w = Workout(userId: uid, date: .now, type: "Run", durationMinutes: 30, distanceKm: 5.0, timed: true)
-                        do {
-                            try await store.addWorkout(w)
-                            status = "Saved!"
-                            try await load()
-                        } catch {
-                            status = "Error: \(error.localizedDescription)"
-                        }
-                    }
-                }
                 Button("Refresh") {
                     Task { try? await load() }
                 }
-            }
-            Toggle("Timed Workout?", systemImage: "clock", isOn: $tw)
+            
+           Toggle("Timed Workout?", systemImage: "clock", isOn: $tw)
             
             HStack{
                 TextField("Workout", text: $wrk)
@@ -113,7 +130,136 @@ struct WorkoutsView: View {
         .padding()
         .task { try? await load() }
     }
+    
+    //Sheet to add a set based workout
+    struct WorkoutPopup: View{
+        @Environment(\.presentationMode) var presentationMode
+        
+        @State private var status = ""
+        @State private var workouts: [Workout] = []
+        @State private var wrk: String = ""
+        @State private var set: Int = 0
+        @State private var rep: Double = 0.0
+        @State private var tw: Bool = false
+        let store = FirestoreService()
+        
+        var body: some View{
+            ZStack{
+                Color.red
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 12){
+                    
+                    Text("Workout")
+                    TextField("Workout", text: $wrk)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                    Text("Number of Sets")
+                    TextField("Sets", value: $set, format: .number)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    Text("Number of Reps")
+                    TextField("Reps", value: $rep, format: .number)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    HStack{
+                        Button("Add") {
+                            Task {
+                                presentationMode.wrappedValue.dismiss()
+                                guard let uid = Auth.auth().currentUser?.uid else { return }
+                                let w = Workout(userId: uid, date: .now, type: wrk, durationMinutes: set, distanceKm: rep, timed: false)
+                                do {
+                                    try await store.addWorkout(w)
+                                    status = "Saved!"
+                                } catch {
+                                    status = "Error: \(error.localizedDescription)"
+                                }
+                                
+                            }
+                            
+                        }
+                        .foregroundColor(.green)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.white)
+                    Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.black)
+                            .padding(20)
+                    })
+                    }
+                }
+            }
+            
+        }
+    }
 
+    //Sheet to add a time based workout
+    struct TimedWorkoutPopup: View{
+        @Environment(\.presentationMode) var presentationMode
+        
+        @State private var status = ""
+        @State private var workouts: [Workout] = []
+        @State private var wrk: String = ""
+        @State private var dur: Int = 0
+        @State private var dist: Double = 0.0
+        @State private var tw: Bool = true
+        let store = FirestoreService()
+        
+        var body: some View{
+            ZStack{
+                Color.red
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 12){
+                    
+                    Text("Workout")
+                    TextField("Workout", text: $wrk)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                    Text("Duration of Workout")
+                    TextField("Duration", value: $dur, format: .number)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    Text("Distance of Workout")
+                    TextField("Distance", value: $dist, format: .number)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    HStack{
+                        Button("Add") {
+                            Task {
+                                presentationMode.wrappedValue.dismiss()
+                                guard let uid = Auth.auth().currentUser?.uid else { return }
+                                let w = Workout(userId: uid, date: .now, type: wrk, durationMinutes: dur, distanceKm: dist, timed: true)
+                                do {
+                                    try await store.addWorkout(w)
+                                    status = "Saved!"
+                                } catch {
+                                    status = "Error: \(error.localizedDescription)"
+                                }
+                                
+                            }
+                            
+                        }
+                        .foregroundColor(.green)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.white)
+                    Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.black)
+                            .padding(20)
+                    })
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    
     private func load() async throws {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         workouts = try await store.recentWorkouts(for: uid)
